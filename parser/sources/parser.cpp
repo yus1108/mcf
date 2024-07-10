2663f7caea9f8d5b229d4ea09273ec3acf3a6ac8
Un Sung Yoo <yus1108@naver.com>
2024-07-10 17:10:33 +0900

    [feature|parser] Init parser project

---
+++ parser/sources/parser.cpp
@@ -0,0 +1,10 @@
+// parser.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "parser.h"
+
+// TODO: This is an example of a library function
+void fnparser()
+{
+}
