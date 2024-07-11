b62bca909ac697487da94330ede23c6366a73af2
Un Sung Yoo <yus1108@naver.com>
2024-07-11 16:09:19 +0900

    [feature|parser] Implement basic parser

--- parser/includes/parser.h
+++ parser/includes/parser.h
@@ -1,1 +1,26 @@
 #pragma once
+
+namespace mcf
+{
+	namespace ast
+	{
+		class Program;
+	}
+
+	class parser final
+	{
+	public:
+		explicit parser(void) noexcept = delete;
+		explicit parser(const std::string& input) noexcept;
+
+		mcf::ast::program* parse_program( void ) noexcept;
+
+	private:
+		inline void read_next_token(void) noexcept;
+
+	private:
+		mcf::lexer _lexer;
+		mcf::token _currentToken;
+		mcf::token _nextToken;
+	};
+}
--- parser/sources/parser.cpp
+++ parser/sources/parser.cpp
@@ -8,3 +8,23 @@
 void fnparser()
 {
 }
+
+mcf::parser::parser(const std::string& input) noexcept
+	: _lexer(input)
+{
+	_currentToken = _lexer.read_next_token();
+	_nextToken = _lexer.read_next_token();
+}
+
+mcf::ast::program* mcf::parser::parse_program( void ) noexcept
+{
+	ast::program* program = new ast::program();
+	program;
+	return nullptr;
+}
+
+inline void mcf::parser::read_next_token(void) noexcept
+{
+	_currentToken = _nextToken;
+	_nextToken = _lexer.read_next_token();
+}
